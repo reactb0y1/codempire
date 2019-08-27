78936943b9a314b86203f4f009f733c0ce28db44
Alexandr <sashakobzeww@gmail.com>
2019-08-27 20:47:18 +0300

    chr Menu Создана map`изация пунктов меню

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -2,10 +2,31 @@ import React, {Component} from 'react'
 import './Menu.css'
 
 class Menu extends Component {
+    state = {
+        menuItems: [
+            {name: 'Гамбургер'},
+            {name: 'Чизбургер'},
+            {name: 'МакЧикен'},
+            {name: 'ЧикенРол'}
+        ]
+    };
+
+    makeList() {
+        const menuItems = this.state.menuItems;
+
+        const item = menuItems.map((menuItem) =>
+            <li>{menuItem.name}</li>
+        )
+
+        return item
+    }
+
     render() {
         return (
-            <div>
-
+            <div className='menuContainer'>
+                <ul className='Menu'>
+                    {this.makeList()}
+                </ul>
             </div>
         );
     }
