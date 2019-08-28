e4074f69c1ed9e949e5b9b250ed04cbbb452e0ff
Alexandr <sashakobzeww@gmail.com>
2019-08-28 19:23:38 +0300

    chr App Создан роутер

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Menu from './Menu/Menu'
+import Edit from './Menu/Edit/Edit'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <Menu></Menu>
+      <Switch>
+        <Route exact path='/' component={Menu}/>
+        <Route path='/edit' component={Edit}/>
+        {/*<Route path='order' component={Order}/>*/}
+      </Switch>
   );
 }
 
