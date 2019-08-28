75a4679aa74f21619436fac6b21417ce56582030
Alexandr <sashakobzeww@gmail.com>
2019-08-28 20:56:32 +0300

    chr Order Добавлена ссылка из Order в Main

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react'
 import './Order.css'
+import {Link} from 'react-router-dom'
 
 class Order extends Component {
     render() {
         return (
-            <div>
-
-            </div>
+            <Link to='/'>Завершить</Link>
         );
     }
 }
