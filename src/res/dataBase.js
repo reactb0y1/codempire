463dd45eb0a2a2118763796b6932b49c8907c085
Alexandr <sashakobzeww@gmail.com>
2019-08-30 11:59:55 +0300

    chr database Создана БД меню в первом приближении

---
+++ src/res/dataBase.js
@@ -0,0 +1,64 @@
+const menu = [
+    {
+        name: 'Гамбургер',
+        id: 'hamburger',
+        ingredients: [
+            {name: 'Булка', amount: 2},
+            {name: 'Салат', amount: 2},
+            {name: 'Кетчуп', amount: 1},
+            {name: 'Майонез', amount: 1},
+            {name: 'Бекон', amount: 1},
+            {name: 'Огурец', amount: 1},
+            {name: 'Сыр', amount: 0},
+            {name: 'Рыба', amount: 0},
+            {name: 'Курица', amount: 0}
+        ]
+    },
+    {
+        name: 'Чизбургер',
+        id: 'cheeseburger',
+        ingredients: [
+            {name: 'Булка', amount: 2},
+            {name: 'Салат', amount: 2},
+            {name: 'Майонез', amount: 1},
+            {name: 'Сыр', amount: 1},
+            {name: 'Кетчуп', amount: 0},
+            {name: 'Курица', amount: 0},
+            {name: 'Рыба', amount: 0},
+            {name: 'Бекон', amount: 0},
+            {name: 'Огурец', amount: 0}
+        ]
+    },
+    {
+        name: 'Чикенбургер',
+        id: 'chickenburger',
+        ingredients: [
+            {name: 'Булка', amount: 2},
+            {name: 'Салат', amount: 2},
+            {name: 'Кетчуп', amount: 1},
+            {name: 'Майонез', amount: 1},
+            {name: 'Курица', amount: 1},
+            {name: 'Сыр', amount: 0},
+            {name: 'Рыба', amount: 0},
+            {name: 'Бекон', amount: 0},
+            {name: 'Огурец', amount: 0}
+        ]
+    },
+    {
+        name: 'Фишбургер',
+        id: 'fishburger',
+        ingredients: [
+            {name: 'Булка', amount: 2},
+            {name: 'Салат', amount: 2},
+            {name: 'Рыба', amount: 1},
+            {name: 'Кетчуп', amount: 0},
+            {name: 'Майонез', amount: 0},
+            {name: 'Сыр', amount: 0},
+            {name: 'Курица', amount: 0},
+            {name: 'Бекон', amount: 0},
+            {name: 'Огурец', amount: 0}
+        ]
+    },
+]
+
+export default menu
